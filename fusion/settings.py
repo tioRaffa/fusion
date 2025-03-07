5a59a700d6033ac0d515fabde313e4d375bbaad5
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-03-07 16:30:15 -0300

    Update database settings to use PostgreSQL instead of SQLite

--- fusion/settings.py
+++ fusion/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "fusion.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": BASE_DIR / "fusion",
+        "USER": "muniz",
+        "PASSWORD": "32239292",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
