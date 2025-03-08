af725f4f59b7c933ec86aa63d9fc7e9d8f486e62
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-03-08 12:13:11 -0300

    Add ServiceModel and TeamModel with admin registration and initial migration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ServiceModel, TeamModel
 
 # Register your models here.
+
+@admin.register(ServiceModel)
+class ServiceModelAdmin(admin.ModelAdmin):
+    list_display = ['service']
+    ordering = ['-id']
+    
+    
+@admin.register(TeamModel)
+class TeamModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user', 'service', 'active']  
+    list_display_links = ['id', 'user']  
+    list_editable = ['active']  
+    ordering = ['-id']  
--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Base(models.Model):
+    create = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        abstract = True
+        
+        
+        
+class ServiceModel(Base):
+    ICON_CHOICES = (
+        ('lni-cog', 'engrenagem'),
+        ('lni-stats-up', 'grafico'),
+        ('ni-users', 'users'),
+        ('lni-layers', 'design'),
+        ('lni-mobile', 'mobile'),
+        ('lni-rocket', 'rocket'),
+    )
+    
+    service = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    icon = models.CharField(max_length=12, choices=ICON_CHOICES)
+    
+    def __str__(self):
+        return self.service
+    
+    
+class TeamModel(Base):
+    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    service = models.ForeignKey(ServiceModel, on_delete=models.SET_NULL, null=True)
+    pic_profile = models.ImageField(upload_to='fusion/media/%y/%m/%d', blank=True, null=True)
+    facebook = models.CharField(max_length=100, default='#')
+    twitter = models.CharField(max_length=100, default='#')
+    instagram = models.CharField(max_length=100, default='#')
+    
+    def __str__(self):
+        return self.user.first_name
+    
+    
